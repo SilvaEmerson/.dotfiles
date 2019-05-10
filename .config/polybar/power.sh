392e1cb5ef1abccb24ff86ab1f2875a6cda39ca7
Émerson Silva <emersonjose877@gmail.com>
2019-05-10 18:52:38 -0300

    Add Polybar startup scripts

---
+++ .config/polybar/power.sh
@@ -0,0 +1,5 @@
+echo - | awk "{printf \"%.1f\",                     
+$((                                                                                                                 
+  $(cat /sys/class/power_supply/BAT1/current_now) * 
+  $(cat /sys/class/power_supply/BAT1/voltage_now) 
+)) / 1000000000000 }" ; echo " W"

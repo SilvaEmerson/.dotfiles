da0fe969092351d94aa238c60fccbd40ea2d399d
SilvaEmerson <emersonjose877@gmail.com>
2024-05-29 17:18:55 -0300

    add telescope config

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,30 @@
+return {
+  "nvim-telescope/telescope.nvim",
+  keys = {
+    {
+      "<leader><leader>",
+      function()
+        require("telescope.builtin").find_files()
+      end,
+      desc = "Find files",
+    },
+  },
+  config = function()
+    require("telescope").setup({
+      pickers = {
+        find_files = {
+          no_ignore = true,
+          find_command = {
+            "rg",
+            "--files",
+            "--hidden",
+            "-g",
+            "!**/.git/*",
+            "-g",
+            "!**/node_modules/*",
+          },
+        },
+      },
+    })
+  end,
+}

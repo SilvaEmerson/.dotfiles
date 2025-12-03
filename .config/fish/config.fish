c5552d2aa22157a8d134c6b4958cbc2388b33544
SilvaEmerson <emersonjose877@gmail.com>
2025-12-03 18:47:52 -0300

    feat: add java fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,18 +6,21 @@ fish_vi_key_bindings
 alias docker-clean-dangling-images='docker rmi -f (docker images -f "dangling=true" -aq)'
 
 export MISE_DATA_DIR="/mnt/EJS/MiseData"
-export DOCKER_BUILDKIT=1 
+export DOCKER_BUILDKIT=1
 export COMPOSE_DOCKER_CLI_BUILD=1
 export EDITOR=nvim
 export GIT_EXTERNAL_DIFF=difft
 export FZF_TMUX=1
+export JAVA_HOME=/mnt/EJS/graalvm-jdk-25+37.1
 
 # path
 export PATH="$PATH:$HOME/.local/bin"
 export PATH="$PATH:$HOME/.cargo/bin"
+export PATH="$PATH:$HOME/.nix-profile/bin"
+export PATH="$PATH:$JAVA_HOME/bin"
 
 if status is-interactive
-  atuin init fish | source
+    atuin init fish | source
 end
 
 kubectl completion fish | source
